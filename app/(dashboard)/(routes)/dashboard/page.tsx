c6d85b4b02e83ab1e15b3086501d9b111fcc5332
Jatin Hazrati <jatin.hazrati@intimetec.com>
2023-08-13 10:30:46 +0530

    User Authentication using Clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <div>Dashboard Page (Protected)</div>;
+  return (
+    <div>
+      Dashboard Page (Protected)
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
