c35990c294e0fa4bf1559d139f82d427549fbe23
jatinhazrati <jatinhazrati@gmail.com>
2023-08-30 17:00:00 +0530

    Trying different commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sagacity AI",
-  description: "AI Platform",
+  description: "AI SaaS Platform",
 };
 
 export default function RootLayout({
